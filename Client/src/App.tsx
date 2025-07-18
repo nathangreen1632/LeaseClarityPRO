d88684b3eb02cbeb6f9d3782863f4ea0a08cfa13
Nathan Green <ngreen1632@gmail.com>
2025-07-18 08:40:58 -0500

    feat(App): add Footer component for improved layout consistency
    - Include the Footer component in `App.tsx` for consistent application structure
    - Enhance overall user experience by standardizing layout elements

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar.js';
 import AppRoutes from './AppRoutes.js';
+import Footer from "./components/Footer.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
   );
 }
